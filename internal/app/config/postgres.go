e4065e6cc7654f0a14af5f0d49378d79db02b539
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-03 09:12:57 +0700

    chore(db): initialize database

--- internal/app/config/postgres.go
+++ internal/app/config/postgres.go
@@ -1,43 +1,28 @@
 package config
 
 import (
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"log"
 	"time"
 )
 
-func NewPostgres(cfg *Config) *gorm.DB {
+func NewPostgres(cfg *Config) *sqlx.DB {
 	var (
-		dsn          = cfg.Get("PG_DSN")
-		connOpen     = cfg.GetInt("PG_CONN_OPEN", 100)
-		connIdle     = cfg.GetInt("PG_CONN_IDLE", 10)
-		connLifeTime = cfg.GetInt("PG_CONN_LIFETIME", 15)
+		dsn          = cfg.Get("DB_DSN", "postgres://user:password@localhost:5432/postgres")
+		connOpen     = cfg.GetInt("DB_CONN_OPEN", 100)
+		connIdle     = cfg.GetInt("DB_CONN_IDLE", 10)
+		connLifeTime = cfg.GetInt("DB_CONN_LIFETIME", 15)
 	)
 
-	dialect := postgres.Open(dsn)
-
-	loggerConfig := logger.Default.LogMode(logger.Info) // Info on development, Error on production
-
-	gormConfig := &gorm.Config{
-		Logger:         loggerConfig,
-		TranslateError: true,
-	}
-
-	db, err := gorm.Open(dialect, gormConfig)
+	db, err := sqlx.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	psql, err := db.DB()
-	if err != nil {
-		log.Fatalf("Failed to create postgres instance: %v", err)
-	}
-
-	psql.SetMaxOpenConns(connOpen)
-	psql.SetMaxIdleConns(connIdle)
-	psql.SetConnMaxLifetime(time.Minute * time.Duration(connLifeTime))
+	db.SetMaxOpenConns(connOpen)
+	db.SetMaxIdleConns(connIdle)
+	db.SetConnMaxLifetime(time.Minute * time.Duration(connLifeTime))
 
 	return db
 }
