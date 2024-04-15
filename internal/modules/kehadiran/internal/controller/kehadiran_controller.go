d4f7095c23edee00dc08a2331d804eea7d5cc83e
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-15 20:35:43 +0700

    fix(#15): utilize `sql.NullTime` for nullable time

--- internal/modules/kehadiran/internal/controller/kehadiran_controller.go
+++ internal/modules/kehadiran/internal/controller/kehadiran_controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/ikti-its/khanza-api/internal/app/config"
 	"github.com/ikti-its/khanza-api/internal/app/exception"
+	"github.com/ikti-its/khanza-api/internal/app/helper"
 	web "github.com/ikti-its/khanza-api/internal/app/model"
 	"github.com/ikti-its/khanza-api/internal/modules/kehadiran/internal/model"
 	"github.com/ikti-its/khanza-api/internal/modules/kehadiran/internal/usecase"
@@ -37,7 +38,8 @@ func (c *KehadiranController) Attend(ctx *fiber.Ctx) error {
 		})
 	}
 
-	response := c.UseCase.Attend(&request)
+	updater := ctx.Locals("user").(string)
+	response := c.UseCase.Attend(&request, updater)
 
 	return ctx.Status(fiber.StatusCreated).JSON(web.Response{
 		Code:   fiber.StatusCreated,
@@ -73,14 +75,25 @@ func (c *KehadiranController) Leave(ctx *fiber.Ctx) error {
 }
 
 func (c *KehadiranController) Get(ctx *fiber.Ctx) error {
-	// TODO: Implement GetByTanggal in here
-	response := c.UseCase.Get()
+	tanggal := ctx.Query("tanggal")
 
-	return ctx.Status(fiber.StatusOK).JSON(web.Response{
-		Code:   fiber.StatusOK,
-		Status: "OK",
-		Data:   response,
-	})
+	if tanggal != "" && !helper.ParseTime(tanggal, "2006-01-02").IsZero() {
+		response := c.UseCase.GetByTanggal(tanggal)
+
+		return ctx.Status(fiber.StatusOK).JSON(web.Response{
+			Code:   fiber.StatusOK,
+			Status: "OK",
+			Data:   response,
+		})
+	} else {
+		response := c.UseCase.Get()
+
+		return ctx.Status(fiber.StatusOK).JSON(web.Response{
+			Code:   fiber.StatusOK,
+			Status: "OK",
+			Data:   response,
+		})
+	}
 }
 
 func (c *KehadiranController) GetByPegawaiId(ctx *fiber.Ctx) error {
@@ -99,33 +112,6 @@ func (c *KehadiranController) GetById(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 
 	response := c.UseCase.GetById(id)
-
-	return ctx.Status(fiber.StatusOK).JSON(web.Response{
-		Code:   fiber.StatusOK,
-		Status: "OK",
-		Data:   response,
-	})
-}
-
-func (c *KehadiranController) Update(ctx *fiber.Ctx) error {
-	var request model.UpdateKehadiranRequest
-	id := ctx.Params("id")
-
-	if err := ctx.BodyParser(&request); err != nil {
-		panic(&exception.BadRequestError{
-			Message: "Invalid request body",
-		})
-	}
-
-	if err := c.Validator.Validate(&request); err != nil {
-		message := c.Validator.Message(err)
-		panic(&exception.BadRequestError{
-			Message: message,
-		})
-	}
-
-	updater := ctx.Locals("user").(string)
-	response := c.UseCase.Update(&request, id, updater)
 
 	return ctx.Status(fiber.StatusOK).JSON(web.Response{
 		Code:   fiber.StatusOK,
