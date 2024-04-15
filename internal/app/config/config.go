b0f296d014edc358e34017e7d073407023dc888c
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-15 16:35:56 +0700

    chore(config): set util endpoints

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -28,6 +28,16 @@ func (c *Config) GetInt(key string, def int) int {
 	return value
 }
 
+func (c *Config) GetBool(key string, def bool) bool {
+	value, err := strconv.ParseBool(os.Getenv(key))
+	if err != nil {
+		log.Printf("Failed to parse %s to bool, using default value: %t", key, def)
+		return def
+	}
+
+	return value
+}
+
 func NewConfig() *Config {
 	return &Config{}
 }
--- internal/app/config/fiber.go
+++ internal/app/config/fiber.go
@@ -1,15 +1,18 @@
 package config
 
 import (
+	"fmt"
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/ikti-its/khanza-api/internal/app/exception"
 )
 
-func NewFiber() *fiber.App {
+func NewFiber(cfg *Config) *fiber.App {
 	config := fiber.Config{
 		ErrorHandler: exception.Handler,
 		JSONEncoder:  json.Marshal,
@@ -19,24 +22,38 @@ func NewFiber() *fiber.App {
 	app.Use(cors.New())    // Enable CORS
 	app.Use(recover.New()) // Recover panics outside fiber
 
-	log := logger.Config{
-		Format: "[${ip}]:${port} ${status} - ${method} ${path} [${latency}]\n",
+	app.Get("/", func(ctx *fiber.Ctx) error { // Home Route
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "Endpoint `/` is not set. Please refer to the API documentation at https://documenter.getpostman.com/view/23649536/2sA2rDy1iF",
+		})
+	})
+
+	app.Use(favicon.New(favicon.Config{ // Favicon
+		File: fmt.Sprintf("%s/img/favicon.ico", cfg.Get("APP_STORAGE", "storage")), // e.g. "storage/img/favicon.ico"
+		URL:  "/favicon.ico",
+	}))
+
+	app.Use(healthcheck.New(healthcheck.Config{ // Health Check
+		LivenessProbe: func(ctx *fiber.Ctx) bool {
+			return true
+		},
+		LivenessEndpoint: "/healthz",
+	}))
+
+	if cfg.GetBool("APP_DEBUG", false) { // Log requests [Only for development, remove in production]
+		log := logger.Config{
+			Format:     "[${time}] ${status} - ${method} ${path}\n", // e.g. [2006-01-02 15:04:05] 200 - GET /
+			TimeFormat: "2006-01-02 15:04:05",
+			TimeZone:   "Asia/Jakarta",
+		}
+		app.Use(logger.New(log))
 	}
-	app.Use(logger.New(log)) // Log requests [Only for development, remove in production]
 
-	// Default Route
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
-			"message": "Endpoint `/` is not set. Please refer to the API documentation: https://documenter.getpostman.com/view/23649536/2sA2rDy1iF",
+	if cfg.GetBool("APP_DDOS", false) { // Sinkhole if under-attack [Only for development, remove in production]
+		app.Get("/play.mp3", func(ctx *fiber.Ctx) error { // Sinkhole Route
+			return ctx.Redirect("https://www.youtube.com/watch?v=4hVhXKl-xyY", fiber.StatusMovedPermanently) // Rickroll :D
 		})
-	})
-
-	// Health Check
-	app.Get("/healthz", func(ctx *fiber.Ctx) error {
-		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
-			"status": "healthy",
-		})
-	})
+	}
 
 	return app
 }
