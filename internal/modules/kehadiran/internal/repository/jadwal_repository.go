5082e08130e631252fdcf64692936f9055058c08
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-20 02:15:28 +0700

    feat(kehadiran): jadwal pagination endpoint

--- internal/modules/kehadiran/internal/repository/jadwal_repository.go
+++ internal/modules/kehadiran/internal/repository/jadwal_repository.go
@@ -7,6 +7,7 @@ import (
 
 type JadwalRepository interface {
 	Find() ([]entity.Jadwal, error)
+	FindPage(page, size int) ([]entity.Jadwal, int, error)
 	FindByHariId(id int) ([]entity.Jadwal, error)
 	FindByPegawaiId(id uuid.UUID) ([]entity.Jadwal, error)
 	FindById(id uuid.UUID) (entity.Jadwal, error)
