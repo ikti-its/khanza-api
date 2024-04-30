15cd30f19ce42865315ec107d76815c8c5eae7ac
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-30 18:03:57 +0700

    feat(mobile): revamp profile update endpoint

--- internal/modules/mobile/internal/model/profile_model.go
+++ internal/modules/mobile/internal/model/profile_model.go
@@ -6,8 +6,10 @@ type ProfileRequest struct {
 	Email     string  `json:"email" validate:"required"`
 	Password  string  `json:"password" validate:"required,min=6,max=20"`
 	Alamat    string  `json:"alamat" validate:"required"`
-	AlamatLat float64 `json:"alamat_lat" validate:"required"`
-	AlamatLon float64 `json:"alamat_lon" validate:"required"`
+	AlamatLat float64 `json:"alamat_lat" validate:"required,latitude"`
+	AlamatLon float64 `json:"alamat_lon" validate:"required,longitude"`
+	Kota      string  `json:"kota" validate:"required"`
+	KodePos   string  `json:"kode_pos" validate:"required,max=5"`
 }
 
 type ProfileResponse struct {
@@ -17,4 +19,6 @@ type ProfileResponse struct {
 	Alamat    string  `json:"alamat"`
 	AlamatLat float64 `json:"alamat_lat"`
 	AlamatLon float64 `json:"alamat_lon"`
+	Kota      string  `json:"kota"`
+	KodePos   string  `json:"kode_pos"`
 }
