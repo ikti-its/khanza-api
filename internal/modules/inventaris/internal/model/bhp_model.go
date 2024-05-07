e39c638d40ac0b42461eb607b13e70604ae88f3f
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-05-07 17:29:15 +0700

    feat(inventaris): update bhp endpoint

--- internal/modules/inventaris/internal/model/bhp_model.go
+++ internal/modules/inventaris/internal/model/bhp_model.go
@@ -2,7 +2,6 @@ package model
 
 type BhpRequest struct {
 	IdMedis     string `json:"id_barang_medis" validate:"required,uuid4"`
-	Satuan      string `json:"satuan" validate:"required,oneof=pasang kotak paket item botol tabung ml"`
 	Jumlah      int    `json:"jumlah" validate:"required,numeric"`
 	Kadaluwarsa string `json:"kadaluwarsa" validate:"required"`
 }
@@ -10,7 +9,6 @@ type BhpRequest struct {
 type BhpResponse struct {
 	Id          string `json:"id"`
 	IdMedis     string `json:"id_barang_medis"`
-	Satuan      string `json:"satuan"`
 	Jumlah      int    `json:"jumlah"`
 	Kadaluwarsa string `json:"kadaluwarsa"`
 }
