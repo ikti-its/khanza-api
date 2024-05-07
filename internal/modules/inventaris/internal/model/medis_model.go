6632be736a85cfa6f9d379fc3838cbe42afb1906
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-05-07 17:30:02 +0700

    feat(inventaris): update barang medis endpoint

--- internal/modules/inventaris/internal/model/medis_model.go
+++ internal/modules/inventaris/internal/model/medis_model.go
@@ -1,18 +1,20 @@
 package model
 
 type MedisRequest struct {
-	Nama  string  `json:"nama" validate:"required"`
-	Jenis string  `json:"jenis" validate:"required,oneof=Obat 'Alat Kesehatan' 'Bahan Habis Pakai' Darah"`
-	Harga float64 `json:"harga" validate:"numeric"`
-	Stok  int     `json:"stok" validate:"numeric"`
+	Nama   string  `json:"nama" validate:"required"`
+	Jenis  string  `json:"jenis" validate:"required,oneof=Obat 'Alat Kesehatan' 'Bahan Habis Pakai' Darah"`
+	Satuan int     `json:"satuan" validate:"required,numeric"`
+	Harga  float64 `json:"harga" validate:"numeric"`
+	Stok   int     `json:"stok" validate:"numeric"`
 }
 
 type MedisResponse struct {
-	Id    string  `json:"id"`
-	Nama  string  `json:"nama"`
-	Jenis string  `json:"jenis"`
-	Harga float64 `json:"harga"`
-	Stok  int     `json:"stok"`
+	Id     string  `json:"id"`
+	Nama   string  `json:"nama"`
+	Jenis  string  `json:"jenis"`
+	Satuan int     `json:"satuan"`
+	Harga  float64 `json:"harga"`
+	Stok   int     `json:"stok"`
 }
 
 type MedisPageResponse struct {
