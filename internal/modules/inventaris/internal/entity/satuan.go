085f009bb377eb52b1c6820d28dcbbc2c7ed01a5
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-05-07 17:28:37 +0700

    feat(inventaris): satuan endpoint

---
+++ internal/modules/inventaris/internal/entity/satuan.go
@@ -0,0 +1,6 @@
+package entity
+
+type Satuan struct {
+	Id   int    `db:"id"`
+	Nama string `db:"nama"`
+}
---
+++ internal/modules/inventaris/internal/model/satuan_model.go
@@ -0,0 +1,6 @@
+package model
+
+type SatuanResponse struct {
+	Id   int    `json:"id"`
+	Nama string `json:"nama"`
+}
