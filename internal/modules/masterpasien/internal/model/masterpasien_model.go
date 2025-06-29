d3323c0db65e600322133b5a45e9bab33ef82477
DynnTzy <didin6428@gmail.com>
2025-06-29 08:26:55 +0700

    feat: masterpasien api

--- internal/modules/masterpasien/internal/model/masterpasien_model.go
+++ internal/modules/masterpasien/internal/model/masterpasien_model.go
@@ -1,37 +1,44 @@
 package model
 
-import "time"
+
 
 type Model struct {
-	NoRM             string    `json:"no_rkm_medis" db:"no_rkm_medis"`
-	NamaPasien       string    `json:"nama_pasien" db:"nm_pasien"`
-	NoKTP            string    `json:"no_ktp" db:"no_ktp"`
-	JenisKelamin     string    `json:"jk" db:"jk"`
-	TempatLahir      string    `json:"tmp_lahir" db:"tmp_lahir"`
-	TanggalLahir     time.Time `json:"tgl_lahir" db:"tgl_lahir"`
-	NamaIbu          string    `json:"nm_ibu" db:"nm_ibu"`
+	No_rkm_medis             string    `json:"no_rkm_medis" db:"no_rkm_medis"`
+	Nm_pasien       string    `json:"nm_pasien" db:"nm_pasien"`
+	No_ktp            string    `json:"no_ktp" db:"no_ktp"`
+	Jk     string    `json:"jk" db:"jk"`
+	Tmp_lahir      string    `json:"tmp_lahir" db:"tmp_lahir"`
+	Tgl_lahir     string `json:"tgl_lahir" db:"tgl_lahir"`
+	Nm_ibu          string    `json:"nm_ibu" db:"nm_ibu"`
 	Alamat           string    `json:"alamat" db:"alamat"`
-	GolonganDarah    string    `json:"gol_darah" db:"gol_darah"`
+	Gol_darah    string    `json:"gol_darah" db:"gol_darah"`
 	Pekerjaan        string    `json:"pekerjaan" db:"pekerjaan"`
-	StatusPernikahan string    `json:"stts_nikah" db:"stts_nikah"`
+	Stts_nikah string    `json:"stts_nikah" db:"stts_nikah"`
 	Agama            string    `json:"agama" db:"agama"`
-	TanggalDaftar    time.Time `json:"tgl_daftar" db:"tgl_daftar"`
-	NoTelepon        string    `json:"no_tlp" db:"no_tlp"`
+	Tgl_daftar    string `json:"tgl_daftar" db:"tgl_daftar"`
+	No_tlp        string    `json:"no_tlp" db:"no_tlp"`
 	Umur             string    `json:"umur" db:"umur"`
-	Pendidikan       string    `json:"pnd" db:"pnd"`
-	PenanggungJawab  string    `json:"keluarga" db:"keluarga"`
-	NamaPJ           string    `json:"namakeluarga" db:"namakeluarga"`
-	HubunganPJ       string    `json:"kd_kel" db:"kd_kel"`
-	Asuransi         string    `json:"kd_pj" db:"kd_pj"`
-	NoPeserta        string    `json:"no_peserta" db:"no_peserta"`
-	PekerjaanPJ      string    `json:"pekerjaanpj" db:"pekerjaanpj"`
-	AlamatPJ         string    `json:"alamatpj" db:"alamatpj"`
-	Suku             string    `json:"suku_bangsa" db:"suku_bangsa"`
-	Bahasa           string    `json:"bahasa_pasien" db:"bahasa_pasien"`
-	Instansi         string    `json:"perusahaan_pasien" db:"perusahaan_pasien"`
-	NIPNRP           string    `json:"nip" db:"nip"`
-	Email            string    `json:"email" db:"email"`
-	CacatFisik       string    `json:"cacat_fisik" db:"cacat_fisik"`
+	Pnd       string    `json:"pnd" db:"pnd"`
+	Keluarga  string    `json:"keluarga" db:"keluarga"`
+	Namakeluarga          string    `json:"namakeluarga" db:"namakeluarga"`
+	Kd_pj        string    `json:"kd_pj" db:"kd_pj"`
+	No_peserta        string    `json:"no_peserta" db:"no_peserta"`
+	Kd_kel           string    `json:"kd_kel" db:"kd_kel"`
+	Kd_kec           string    `json:"kd_kec" db:"kd_kec"`
+	Kd_kab           string    `json:"kd_kab" db:"kd_kab"`
+	Pekerjaanpj      string    `json:"pekerjaanpj" db:"pekerjaanpj"`
+	Alamatpj         string    `json:"alamatpj" db:"alamatpj"`
+	Kelurahanpj      string    `json:"kelurahanpj" db:"kelurahanpj"`
+	Kecamatanpj      string    `json:"kecamatanpj" db:"kecamatanpj"`
+	Kabupatenpj      string    `json:"kabupatenpj" db:"kabupatenpj"`
+	Suku_bangsa             string    `json:"suku_bangsa" db:"suku_bangsa"`
+	Bahasa_pasien           string    `json:"bahasa_pasien" db:"bahasa_pasien"`
+	Perusahaan_pasien         string    `json:"perusahaan_pasien" db:"perusahaan_pasien"`
+	Nip                     string    `json:"nip" db:"nip"`
+	Email                   string    `json:"email" db:"email"`
+	Cacat_fisik             string    `json:"cacat_fisik" db:"cacat_fisik"`
+	Kd_prop                 string    `json:"kd_prop" db:"kd_prop"`
+	Propinsipj              string    `json:"propinsipj" db:"propinsipj"`
 }
 
 type Request = Model
--- internal/modules/masterpasien/internal/repository/masterpasien_repository.go
+++ internal/modules/masterpasien/internal/repository/masterpasien_repository.go
@@ -22,40 +22,7 @@ func NewRepository(db *sqlx.DB) Repository {
 }
 
 func (r *RepositoryImpl) FindAll() ([]entity.MasterPasien, error) {
-	query := `
-SELECT 
-  no_rkm_medis,
-  nm_pasien,
-  no_ktp,
-  jk,
-  tmp_lahir,
-  tgl_lahir,
-  nm_ibu,
-  alamat,
-  gol_darah,
-  pekerjaan,
-  stts_nikah,
-  agama,
-  tgl_daftar,
-  no_tlp,
-  umur,
-  pnd,
-  namakeluarga,
-  kd_kel,
-  kd_pj,
-  no_peserta,
-  pekerjaanpj,
-  alamatpj,
-  suku_bangsa,
-  bahasa_pasien,
-  perusahaan_pasien,
-  nip,
-  email,
-  cacat_fisik
-FROM pasien
-ORDER BY no_rkm_medis DESC
-`
-
+	query := `SELECT * FROM pasien ORDER BY no_rkm_medis DESC`
 	var result []entity.MasterPasien
 	err := r.DB.Select(&result, query)
 	return result, err
@@ -70,48 +37,115 @@ func (r *RepositoryImpl) FindById(id string) (entity.MasterPasien, error) {
 
 func (r *RepositoryImpl) Insert(data *entity.MasterPasien) error {
 	query := `
-		INSERT INTO pasien (
-			no_rkm_medis, nm_pasien, no_ktp, jk, tmp_lahir, tgl_lahir,
-			nm_ibu, alamat, gol_darah, pekerjaan, stts_nikah, agama,
-			tgl_daftar, no_tlp, umur, pnd, keluarga, namakeluarga,
-			kd_pj, no_peserta, pekerjaanpj, alamatpj, suku_bangsa, bahasa_pasien,
-			perusahaan_pasien, nip, email, cacat_fisik
-		) VALUES (
-			$1, $2, $3, $4, $5, $6,
-			$7, $8, $9, $10, $11, $12,
-			$13, $14, $15, $16, $17, $18,
-			$19, $20, $21, $22, $23, $24,
-			$25, $26, $27, $28
-		)
-	`
-	_, err := r.DB.Exec(query,
-		data.NoRM, data.NamaPasien, data.NoKTP, data.JenisKelamin, data.TempatLahir, data.TanggalLahir,
-		data.NamaIbu, data.Alamat, data.GolonganDarah, data.Pekerjaan, data.StatusPernikahan, data.Agama,
-		data.TanggalDaftar, data.NoTelepon, data.Umur, data.Pendidikan, data.PenanggungJawab, data.NamaPJ,
-		data.Asuransi, data.NoPeserta, data.PekerjaanPJ, data.AlamatPJ, data.Suku, data.Bahasa,
-		data.Instansi, data.NIPNRP, data.Email, data.CacatFisik,
-	)
+	INSERT INTO pasien (
+		no_rkm_medis, nm_pasien, no_ktp, jk, tmp_lahir, tgl_lahir,
+		nm_ibu, alamat, gol_darah, pekerjaan, stts_nikah, agama,
+		tgl_daftar, no_tlp, umur, pnd, keluarga, namakeluarga,
+		kd_pj, no_peserta, pekerjaanpj, alamatpj, suku_bangsa, bahasa_pasien,
+		perusahaan_pasien, nip, email, cacat_fisik,
+		kd_kel, kd_kec, kd_kab, kelurahanpj, kecamatanpj, kabupatenpj, kd_prop, propinsipj
+	) VALUES (
+		$1, $2, $3, $4, $5, $6,
+		$7, $8, $9, $10, $11, $12,
+		$13, $14, $15, $16, $17, $18,
+		$19, $20, $21, $22, $23, $24,
+		$25, $26, $27, $28,
+		$29, $30, $31, $32, $33, $34, $35, $36
+	)`
+_, err := r.DB.Exec(query,
+	data.No_rkm_medis,
+	data.Nm_pasien,
+	data.No_ktp,
+	data.Jk,
+	data.Tmp_lahir,
+	data.Tgl_lahir,
+	data.Nm_ibu,
+	data.Alamat,
+	data.Gol_darah,
+	data.Pekerjaan,
+	data.Stts_nikah,
+	data.Agama,
+	data.Tgl_daftar,
+	data.No_tlp,
+	data.Umur,
+	data.Pnd,
+	data.Keluarga,
+	data.Namakeluarga,
+	data.Kd_pj,
+	data.No_peserta,
+	data.Pekerjaanpj,
+	data.Alamatpj,
+	data.Suku_bangsa,
+	data.Bahasa_pasien,
+	data.Perusahaan_pasien,
+	data.Nip,
+	data.Email,
+	data.Cacat_fisik,
+	data.Kd_kel,
+	data.Kd_kec,
+	data.Kd_kab,
+	data.Kelurahanpj,
+	data.Kecamatanpj,
+	data.Kabupatenpj,
+	data.Kd_prop,
+	data.Propinsipj,
+)
+
 	return err
 }
 
 func (r *RepositoryImpl) Update(data *entity.MasterPasien) error {
 	query := `
-		UPDATE pasien SET 
-			nm_pasien = $2, no_ktp = $3, jk = $4, tmp_lahir = $5, tgl_lahir = $6,
-			nm_ibu = $7, alamat = $8, gol_darah = $9, pekerjaan = $10, stts_nikah = $11,
-			agama = $12, tgl_daftar = $13, no_tlp = $14, umur = $15, pnd = $16,
-			keluarga = $17, namakeluarga = $18, kd_pj = $19, no_peserta = $20,
-			pekerjaanpj = $21, alamatpj = $22, suku_bangsa = $23, bahasa_pasien = $24,
-			perusahaan_pasien = $25, nip = $26, email = $27, cacat_fisik = $28
-		WHERE no_rkm_medis = $1
-	`
-	_, err := r.DB.Exec(query,
-		data.NoRM, data.NamaPasien, data.NoKTP, data.JenisKelamin, data.TempatLahir, data.TanggalLahir,
-		data.NamaIbu, data.Alamat, data.GolonganDarah, data.Pekerjaan, data.StatusPernikahan, data.Agama,
-		data.TanggalDaftar, data.NoTelepon, data.Umur, data.Pendidikan, data.PenanggungJawab, data.NamaPJ,
-		data.Asuransi, data.NoPeserta, data.PekerjaanPJ, data.AlamatPJ, data.Suku, data.Bahasa,
-		data.Instansi, data.NIPNRP, data.Email, data.CacatFisik,
-	)
+	UPDATE pasien SET 
+		nm_pasien = $2, no_ktp = $3, jk = $4, tmp_lahir = $5, tgl_lahir = $6,
+		nm_ibu = $7, alamat = $8, gol_darah = $9, pekerjaan = $10, stts_nikah = $11,
+		agama = $12, tgl_daftar = $13, no_tlp = $14, umur = $15, pnd = $16,
+		keluarga = $17, namakeluarga = $18, kd_pj = $19, no_peserta = $20,
+		pekerjaanpj = $21, alamatpj = $22, suku_bangsa = $23, bahasa_pasien = $24,
+		perusahaan_pasien = $25, nip = $26, email = $27, cacat_fisik = $28,
+		kd_kel = $29, kd_kec = $30, kd_kab = $31, kelurahanpj = $32,
+		kecamatanpj = $33, kabupatenpj = $34, kd_prop = $35, propinsipj = $36
+	WHERE no_rkm_medis = $1
+`
+_, err := r.DB.Exec(query,
+	data.No_rkm_medis,
+	data.Nm_pasien,
+	data.No_ktp,
+	data.Jk,
+	data.Tmp_lahir,
+	data.Tgl_lahir,
+	data.Nm_ibu,
+	data.Alamat,
+	data.Gol_darah,
+	data.Pekerjaan,
+	data.Stts_nikah,
+	data.Agama,
+	data.Tgl_daftar,
+	data.No_tlp,
+	data.Umur,
+	data.Pnd,
+	data.Keluarga,
+	data.Namakeluarga,
+	data.Kd_pj,
+	data.No_peserta,
+	data.Pekerjaanpj,
+	data.Alamatpj,
+	data.Suku_bangsa,
+	data.Bahasa_pasien,
+	data.Perusahaan_pasien,
+	data.Nip,
+	data.Email,
+	data.Cacat_fisik,
+	data.Kd_kel,
+	data.Kd_kec,
+	data.Kd_kab,
+	data.Kelurahanpj,
+	data.Kecamatanpj,
+	data.Kabupatenpj,
+	data.Kd_prop,
+	data.Propinsipj,
+)
+
 	return err
 }
 
