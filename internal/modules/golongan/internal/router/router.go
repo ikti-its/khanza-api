90a2576e20bed6ea71e697c6330e67520c17ef78
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-07-02 13:53:11 +0700

    Add middleware import

--- internal/modules/golongan/internal/router/router.go
+++ internal/modules/golongan/internal/router/router.go
@@ -2,8 +2,8 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/ikti-its/khanza-api/internal/app/middleware"
 	"github.com/ikti-its/khanza-api/internal/modules/golongan/internal/controller"
-
 )
 
 func Route(app *fiber.App, Controller *controller.Controller) {
